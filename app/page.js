4bb55c9f4aed5954b3d6ff75065c1dd14eea96fa
Kenpachi-Ikari <ronitdey7777@gmail.com>
2025-04-03 00:29:43 +0530

    Add utility functions, button component, and update styles for Tailwind integration along with Shadcn

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
-    <div>HELLO NODE JS</div>
-  );
+    <Button variant="destructive">Destructive</Button>
+  )
 }
